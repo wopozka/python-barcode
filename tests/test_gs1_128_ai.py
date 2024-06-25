76121bc5155ba0f58970755cfe6805a748fde569
Piotr Wawrzyniak <wopozka@gmail.com>
2024-06-25 15:03:09 +0200

    adding correct testcases, temporarry adding version file

--- tests/test_gs1_128_ai.py
+++ tests/test_gs1_128_ai.py
@@ -11,5 +11,5 @@ TEST_VAL_AI_FIXED_L = (
 
 @pytest.mark.parametrize('target, answer', TEST_VAL_AI_FIXED_L)
 def test_get_val_for_ai_with_fixed_length_decimals(target, answer):
-    gs1_128 = Gs1_128_AI('')
+    gs1_128 = Gs1_128_AI('', code_with_ais='')
     assert gs1_128.get_val_for_ai_with_fixed_length_decimals(target[0], target[1]) == answer
